fbf57f98ad6bc36964bae3491e1c5f5b40165ab3
advancevate <alexrybakov070707@gmail.com>
2024-11-18 16:03:52 +0300

    Create rep1 .py

---
+++ rep1 .py
@@ -0,0 +1,34 @@
+#Задание 1
+integer_var = 10
+float_var = 9.6
+string_var = "Hello"
+list_var = [1,2,3,4,5]
+tuple_var = ("red",9)
+dict_var = {"Name": "Vanya", "Age": 20}
+set_var = {1,2,3,4,5}
+bool_var = True
+print(integer_var)
+print(float_var)
+print(string_var)
+print(list_var)
+print(tuple_var)
+print(dict_var)
+print(set_var)
+print(bool_var)
+
+#Задание 2
+name = "Sasha"
+age = 17
+average_grade = 4.4
+favorite_movies = ["1", "2", "3"]
+personal_info = ("surname : Rybakov ","school number : 1598","avg grade : 4.4")
+skills = {"Speed": 812, "strength": 52}
+visited_countries = {"USA","Germany","France"}
+has_driver_license = False
+
+#Задание 3
+CPU = "Intel Core i5"
+RAM = 16
+Speed = 69
+xxx = [CPU, RAM, Speed]
+print(xxx)
