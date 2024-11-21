5239bdec91f6df5c7757696ec5f8fb1253413a53
advancevate <alexrybakov070707@gmail.com>
2024-11-21 10:39:35 +0300

    Create rep7.py

---
+++ rep7.py
@@ -0,0 +1,9 @@
+#Задание 1
+def create_car(brand: str, color: str, max_speed: int) -> str:
+    return(f"марка: {brand}, цвет: {color}, макс скрость: {max_speed} км/ч")
+car_1 = create_car("McLaren", "black", 500)
+car_2 = create_car("Tesla", "white", 270)
+print(car_1)
+print(car_2)
+
+#Задание 2
