750257621de510e79d136dfc38efd6bcec2a7d76
advancevate <alexrybakov070707@gmail.com>
2024-12-03 11:59:19 +0300

    Update rep7.py

--- rep7.py
+++ rep7.py
@@ -7,3 +7,20 @@ print(car_1)
 print(car_2)
 
 #Задание 2
+def switch_check(switch: bool):
+    if switch == True:
+        print("True работает")
+    elif switch == False:
+        print("False не работает")
+    else:
+        print(f"{switch} сломан")
+
+switch1 = True
+switch2 = False
+switch3 = None
+
+switch_check(switch1)
+switch_check(switch2)
+switch_check(switch3)
+
+#Задание 3
